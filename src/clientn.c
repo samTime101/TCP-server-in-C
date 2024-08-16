7102d0baa63655e247b6c34acb2a8451c2366056
SamTime101 <116491957+samTime101@users.noreply.github.com>
2024-08-16 16:41:43 +0545

    updated * to more redeable code

--- src/clientn.c
+++ src/clientn.c
@@ -9,76 +9,82 @@
 #include <netdb.h>
 #define MAX 1024
 
-
 void *recv_func(void *socket_desc) {
-    int clientfd = *(int *)socket_desc;
+    int clientfd ;
+    memcpy(&clientfd,socket_desc, sizeof(clientfd));
     char rcv[MAX];
     int recr;
     while ((recr = recv(clientfd, rcv, sizeof(rcv) - 1, 0)) > 0) {
+        if (recr < 0) {
+            perror("Error receiving message");
+            break;
+        }
         rcv[recr] = '\0';
         printf("%s", rcv);
         memset(rcv, 0, sizeof(rcv));
     }
-    if(recr < 0){
-	    perror("receiving failed\n");
+    if (recr < 0) {
+        perror("Receiving failed");
     }
-    /*
-    Closing the clientfd and thread
-    */
     close(clientfd);
     pthread_exit(NULL);
 }
 
 void *send_func(void *socket_desc) {
-    int clientfd = *(int *)socket_desc;
+    int clientfd;
+    memcpy(&clientfd,socket_desc, sizeof(clientfd));
     char snd[MAX];
     while (1) {
         if (fgets(snd, sizeof(snd), stdin) == NULL) {
             break;
         }
         if (send(clientfd, snd, strlen(snd), 0) < 0) {
-            perror("send failed");
+            perror("Send failed");
             break;
         }
         if (strcmp(snd, "exit\n") == 0) {
             printf("EXIT REQUESTED\n");
             break;
         }
-	/*
-	clearing the send buffer
-    */
-	memset(snd, 0, sizeof(snd));
+        memset(snd, 0, sizeof(snd));
     }
     close(clientfd);
     pthread_exit(NULL);
 }
 
 int main(int argc , char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
+        return 1;
+    }
+
     int clientfd;
     struct sockaddr_in server;
     struct hostent *serverip;
-    /*
-    create a socket
-    */
+
     clientfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientfd < 0) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
     serverip = gethostbyname(argv[1]);
     if (serverip == NULL) {
-        printf("No such host\n");
-        return -1;
+        fprintf(stderr, "No such host\n");
+        return 1;
     }
+
     server.sin_family = AF_INET;
     server.sin_port = htons(atoi(argv[2]));
-//    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-     memcpy((char *)&server.sin_addr.s_addr,serverip->h_addr_list[0],serverip->h_length);
-    // Connect to server
+    memcpy(&server.sin_addr.s_addr, serverip->h_addr_list[0], serverip->h_length);
+
     if (connect(clientfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("connect failed");
+        perror("Connect failed");
         return 1;
     }
 
     pthread_t recv_thread, send_thread;
 
-    // Create threads for receiving and sending messages
     pthread_create(&recv_thread, NULL, recv_func, &clientfd);
     pthread_create(&send_thread, NULL, send_func, &clientfd);
 
